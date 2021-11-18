74b134f9bdc19fb50d881c7c415632b5938c9cb4
filka657 <72990504+filka657@users.noreply.github.com>
2021-11-18 10:44:39 +0300

    Add files via upload

---
+++ PZ_4.cpp
@@ -0,0 +1,48 @@
+﻿
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+map <string, int> dict;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string str;
+    cout << "Напишите текст: ";
+    getline(cin, str);
+    str += " ";
+    int i = 0;
+    string tmp;
+    for (int j = 0; j < str.size(); j++)
+    {
+        if (str[j] == ' ' || str[j] == '/0')
+        {
+            tmp = str.substr(i, j - i);
+            //cout << tmp << endl;
+            i = j + 1;
+            if (!tmp.empty())
+            {
+                if (dict.find(tmp) != dict.end())
+                {
+                    dict[tmp]++;
+                }
+                else
+                {
+                    dict.insert(make_pair(tmp, 1));
+                }
+            }
+        }
+    }
+    vector<pair<unsigned int, string>>vect;
+
+    for (pair<string, unsigned int> a : dict)
+        vect.push_back(make_pair(a.second, a.first));
+
+    sort(vect.begin(), vect.end());
+    cout << "Список:\n" << endl;
+    for (int i = 0; i < vect.size(); i++)
+        cout << vect[vect.size() - i - 1].second << ": " << vect[vect.size() - i - 1].first << endl;
+    return 0;
+}
